a69134e683556764dc6c6b0de37aaa4c8d89f9d1
SANADA Euki <yuu.mat.930@gmail.com>
2026-09-01 13:26:35 +0900

    fix: Delete the pre-refactoring files that I’d kept just in case

--- gesture-detection/fanning_detector.py
+++
@@ -1,9 +0,0 @@
-"""Backward-compatible command-line entry point."""
-
-import multiprocessing as mp
-
-from gesture_detection.app import main
-
-if __name__ == "__main__":
-    mp.freeze_support()
-    main()
