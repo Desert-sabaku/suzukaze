a7132f0673715b51af39e1bd87a0bb50a8fd372f
SANADA Euki <yuu.mat.930@gmail.com>
2026-09-22 14:57:07 +0900

    fix: set the path

--- unity_bridge/main.py
+++ unity_bridge/main.py
@@ -1,9 +1,10 @@
 from multiprocessing import freeze_support
 
-# def run_gesture_detection() -> None:
-#     from gesture_detection.app import main as run_application
 
-#     run_application()
+def run_gesture_detection() -> None:
+    from gesture_detection.app import main as run_application
+
+    run_application()
 
 
 def run_unity_bridge() -> None:
@@ -19,7 +20,7 @@ def main() -> None:
 
     if mode in {"1", "gesture", "gesture_detection"}:
         freeze_support()
-        # run_gesture_detection()
+        run_gesture_detection()
     elif mode in {"2", "unity", "unity_bridge"}:
         run_unity_bridge()
     else:
